b46c72b3b7b0903acbc37725291a1487c33d80ae
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-10-31 21:36:26 +0400

    update banner photo

--- components/banner-image.tsx
+++ components/banner-image.tsx
@@ -1,17 +1,18 @@
 "use client"
 import React from 'react';
-import * as AspectRatio from '@radix-ui/react-aspect-ratio';
+import Banner from '../public/products/tenzity-banner.jpg'
+import Image from 'next/image';
 
 const BannerImage = () => (
     <div className="Container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
-
-        <div className="Container" style={{ width: '90%', height: 'auto', }}>
-            <img
+        <div className="Container" style={{ width: '98%', height: 'auto', }}>
+            <Image
                 className="Image"
-                src="https://img.freepik.com/vecteurs-libre/boutique-mode-illustration-vectorielle-plane-vetements-mode_1284-77655.jpg?w=2000"
+                src={Banner}
                 alt="Landscape photograph by Tobias Tullius"
                 style={{ width: '100%', height: '400px', objectFit: 'cover', borderRadius: '15px', }}
             />
+
         </div>
 
 
